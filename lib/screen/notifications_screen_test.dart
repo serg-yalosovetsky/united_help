01caca043134ab4c05e97542cee2ad1a962367f7
syalosovetskyi <43996021+syalosovetskyi@users.noreply.github.com>
2022-12-25 22:20:41 +0200

    push notifications PoC

--- lib/screen/notifications_screen_test.dart
+++ lib/screen/notifications_screen_test.dart
@@ -1,11 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
 import 'package:overlay_support/overlay_support.dart';
+import 'package:provider/provider.dart';
+import 'package:united_help/routes/routes.dart';
+import 'package:united_help/screen/new_event_screen.dart';
 
 import '../constants/colors.dart';
 import '../fragment/bottom_navbar.dart';
 import '../fragment/build_app_bar.dart';
+import '../fragment/welcome_button.dart';
+import '../services/appservice.dart';
 
 
 Future _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
